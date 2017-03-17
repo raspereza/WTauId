7a3466f74072bfe71c60802cc810f7fd51c125a2
Teresa <teresa.lenz@desy.de>
2017-03-17 08:52:02 +0100

    hadd on subsets removed

--- NTuples/hadd.sh
+++ NTuples/hadd.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-for dir in $(find -maxdepth 1 -type d -name "*")
+for dir in $(find -maxdepth 1 -type d -name "W*JetsToLNu*")
 do
     filename=${dir:2:${#dir}-8}
     echo $filename
-    for i in {0..9}
-    do
-	hadd -f ${filename}_${i}.root ${filename}_files/*${i}.root
-    done
-    hadd -f ${filename}.root ${filename}_*.root
-    rm ${filename}_*.root
+    #for i in {0..9}
+    #do
+    #	hadd -f ${filename}_${i}.root ${filename}_files/*${i}.root
+    #done
+    #hadd -f ${filename}.root ${filename}_*.root
+    hadd -f ${filename}.root ${filename}_files/*.root
+    #rm ${filename}_*.root
 done
 
