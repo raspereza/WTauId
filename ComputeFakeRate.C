4482d267e1d329190369123e95d3ccd86d1e47b0
Teresa <teresa.lenz@desy.de>
2017-03-29 18:18:51 +0200

    new binning + with/without trigger comparison

--- ComputeFakeRate.C
+++ ComputeFakeRate.C
@@ -1,5 +1,6 @@
 #include "HttStylesNew.cc"
 #include "TStyle.h"
+#include "TF2.h"
 #include "settings.h"
 
 void ComputeFakeRate() {
@@ -11,11 +12,12 @@ void ComputeFakeRate() {
   SetStyle();
   TH1::SetDefaultSumw2();
   TH2::SetDefaultSumw2();
-  const int nBinsRatio = 6;
-  double binsRatio[nBinsRatio+1] = {0 , 0.6 , 0.7 , 0.8 , 0.9 , 0.95 , 2.};
+  const int nBinsRatio =4;
+  double binsRatio[nBinsRatio+1] = { 0.0 , 0.75 , 0.825 , 0.9 , 2. };
   const int nBinsJetPt = 3;
-  double binsJetPt[nBinsJetPt+1] = {0 , 175 , 250 , 1000};
+  double binsJetPt[nBinsJetPt+1] = {100 , 150 , 200 , 1200};
   TH2D* h_fakerate_2d = new TH2D("h_fakerate_2d","h_fakerate_2d",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
+  TH2D* h_fakerate_2d_woTrig = new TH2D("h_fakerate_2d_woTrig","h_fakerate_2d_woTrig",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
 
   std::vector< std::pair<TString,std::vector<TString>> > samples;
   std::vector<TString> data_SingleMuon;
@@ -23,11 +25,6 @@ void ComputeFakeRate() {
   std::vector<TString> data_JetHT;
   data_JetHT.push_back("JetHT_Run2016");
   std::vector<TString> wjets;
-  //wjets.push_back("WJetsToLNu_13TeV-madgraphMLM");
-  //wjets.push_back("W1JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8");
-  //wjets.push_back("W2JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8");
-  //wjets.push_back("W3JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8");
-  //wjets.push_back("W4JetsToLNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8");
   wjets.push_back("WJetsToLNu_HT-70To100_13TeV-madgraphMLM-pythia8");
   wjets.push_back("WJetsToLNu_HT-100To200_13TeV-madgraphMLM-pythia8");
   wjets.push_back("WJetsToLNu_HT-200To400_13TeV-madgraphMLM-pythia8");
@@ -36,8 +33,8 @@ void ComputeFakeRate() {
   wjets.push_back("WJetsToLNu_HT-800To1200_13TeV-madgraphMLM-pythia8");
   
   samples.push_back(make_pair("WJetsToLNu_13TeV-madgraphMLM" , wjets));
-  //samples.push_back(make_pair("SingleMuon_Run2016" , data_SingleMuon));
-  //samples.push_back(make_pair("JetHT_Run2016" , data_JetHT));
+  samples.push_back(make_pair("SingleMuon_Run2016" , data_SingleMuon));
+  samples.push_back(make_pair("JetHT_Run2016" , data_JetHT));
 
   for (unsigned int i=0; i<samples.size(); ++i) {
 
@@ -50,6 +47,8 @@ void ComputeFakeRate() {
       // filling histograms
       TH2D* h_den = new TH2D(samples[i].first+"_"+iso[idx_iso]+"_den",samples[i].first+"_"+iso[idx_iso]+"_den",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
       TH2D* h_num = new TH2D(samples[i].first+"_"+iso[idx_iso]+"_num",samples[i].first+"_"+iso[idx_iso]+"_num",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
+      TH2D* h_den_woTrig = new TH2D(samples[i].first+"_"+iso[idx_iso]+"_den_woTrig",samples[i].first+"_"+iso[idx_iso]+"_den_woTrig",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
+      TH2D* h_num_woTrig = new TH2D(samples[i].first+"_"+iso[idx_iso]+"_num_woTrig",samples[i].first+"_"+iso[idx_iso]+"_num_woTrig",nBinsRatio,binsRatio,nBinsJetPt,binsJetPt);
 
       TString var1 = "tauPt";
       TString var2 = "tauJetPt";
@@ -57,11 +56,22 @@ void ComputeFakeRate() {
       for(unsigned int idx_list=0; idx_list<samples[i].second.size(); idx_list++){
 	cout<<"---------- Sample "<<samples[i].second[idx_list]<<" processing. ---------- "<<endl;
 	selectionCuts select = cr_fakerate_num;
+	cr_fakerate_dijet_num.pfJetTrigger = true;
 	if(samples[i].second[idx_list].Contains("JetHT")) select =  cr_fakerate_dijet_num;
-	makeSelection(dir+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,h_num,var1,var2,var3);
+ 	makeSelection(dir+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,h_num,var1,var2,var3);
 	select = cr_fakerate_den;
+	cr_fakerate_dijet_den.pfJetTrigger = true;
 	if(samples[i].second[idx_list].Contains("JetHT")) select =  cr_fakerate_dijet_den;
 	makeSelection(dir+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,h_den,var1,var2,var3);
+	
+	// wo Trigger
+	cr_fakerate_dijet_num.pfJetTrigger = false;
+	if(samples[i].second[idx_list].Contains("JetHT")) select =  cr_fakerate_dijet_num;
+	makeSelection(dir+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,h_num_woTrig,var1,var2,var3);
+	select = cr_fakerate_den;
+	cr_fakerate_dijet_den.pfJetTrigger = false;
+ 	if(samples[i].second[idx_list].Contains("JetHT")) select =  cr_fakerate_dijet_den;
+	makeSelection(dir+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,h_den_woTrig,var1,var2,var3);
       }
 
       double numE = 0;
@@ -71,7 +81,18 @@ void ComputeFakeRate() {
 
       cout<<samples[i].first<<" : "<<num<<"/"<<den<<" = "<<num/den<<" +/- "<<numE/den<<" (nevents = "<<h_num->GetEntries()<<"/"<<h_den->GetEntries()<<")"<<endl<<endl;
       
+      // Statistical precision of fakerate:
+      for(int i=1; i<=h_num->GetNbinsX(); i++)
+	{for(int j=1; j<=h_num->GetNbinsY(); j++)
+	    {
+	      double numE_bin = 0;
+	      double num_bin = h_num -> IntegralAndError(i,i,j,j,numE_bin);
+	      cout<<"Content in "<<i<<". x-bin and "<<j<<". y-bin : "<<num_bin<<" +/- "<<numE_bin<<endl;	      
+	    }
+	}
+
       h_fakerate_2d -> Divide(h_num,h_den);
+      h_fakerate_2d_woTrig -> Divide(h_num_woTrig,h_den_woTrig);
 
       h_fakerate_2d->GetXaxis()->SetTitle("pt (tau) / pt (jet faking the tau)");
       h_fakerate_2d->GetYaxis()->SetTitle("pt (jet faking the tau)");
@@ -81,6 +102,18 @@ void ComputeFakeRate() {
       h_fakerate_2d->SetMaximum(1);
       h_fakerate_2d->SetMinimum(0);
 
+      // Fit TH2D with double exp function
+      /*
+      TF2* f2d = new TF2("f2d","[0]*exp([1]*x)*exp([2]*y)");
+      h_fakerate_2d->Fit("f2d");
+      cout<<endl;
+      cout<<"chi2 = "<<f2d->GetChisquare()<<endl;
+      cout<<"ndof = "<<f2d->GetNDF()<<endl;
+      cout<<"par0 = "<<f2d->GetParameter(0)<<" +/- "<<f2d->GetParError(0)<<endl;
+      cout<<"par1 = "<<f2d->GetParameter(1)<<" +/- "<<f2d->GetParError(1)<<endl;
+      cout<<"par2 = "<<f2d->GetParameter(2)<<" +/- "<<f2d->GetParError(2)<<endl;
+      cout<<endl;
+      */
       // Fit the fakerate
       /*
       TF1* f = new TF1("func","expo(0)");
@@ -110,7 +143,31 @@ void ComputeFakeRate() {
       leg->Draw();
       canv->Update();
       canv->Print("figures/fakerate_"+samples[i].first+"_"+iso[idx_iso]+".png");
-      
+
+      // Make 1d plots dependent on tauPt/tauJetPt 
+      //TH1D* h = h_fakerate_2d -> ProjectionX("fakerate_projection_withTrig",1,nBinsJetPt);
+      TH1D* h = h_fakerate_2d -> ProjectionY("fakerate_projection_withTrig",1,nBinsRatio);
+      TF1* f1 = new TF1("f1","[0]*exp([1]*x)");
+      h->Fit("f1");
+      h->SetMaximum(1.);
+      h->SetMinimum(0.00001);
+      //TH1D* h_woTrig = h_fakerate_2d_woTrig -> ProjectionX("fakerate_projection_woTrig",1,nBinsJetPt);
+      TH1D* h_woTrig = h_fakerate_2d_woTrig -> ProjectionY("fakerate_projection_woTrig",1,nBinsRatio);
+      h_woTrig->SetMaximum(1.);
+      h_woTrig->SetMinimum(0.0001);
+      h_woTrig->SetLineColor(kRed);
+      h_woTrig->SetMarkerColor(kRed);
+      h->Draw();
+      //h_woTrig->Draw("same");
+      canv->SetLogy();
+      TLegend * leg1 = new TLegend(0.45,0.2,0.89,0.4);
+      SetLegendStyle(leg1);
+      leg1->SetHeader(iso[idx_iso]);
+      leg1->AddEntry(h,"with Trigger","lp");
+      leg1->AddEntry(h_woTrig,"without Trigger","lp");
+      leg1->Draw();
+      canv->Print("figures/fakerate_"+samples[i].first+"_"+iso[idx_iso]+"_projectionY.png");
+
       fileOutput->cd("");
       h_fakerate_2d->SetName(iso[idx_iso]+"Iso");
       h_fakerate_2d->Write(iso[idx_iso]+"Iso");
