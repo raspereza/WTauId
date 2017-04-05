1dbeca1bd8579812370fa4fb132a1f17c4feeef3
Teresa <teresa.lenz@desy.de>
2017-04-05 12:33:46 +0200

    script to make final prefit plots

---
+++ TauIDMeasurement.C
@@ -0,0 +1,152 @@
+#include "HttStylesNew.cc"
+#include "CMS_lumi.C"
+#include "settings.h"
+#include "TStyle.h"
+#include "THStack.h"
+#include "TLegend.h"
+
+void TauIDMeasurement() {
+
+  TString dir("NTuples/");
+
+  SetStyle();
+  TH1::SetDefaultSumw2();
+  TH2::SetDefaultSumw2();
+
+  loadWorkingPoints();
+  initCuts();
+  //loadFakeRates("output/WJetsToLNu_13TeV-madgraphMLM_fakeRate.root");
+  loadFakeRates("output/fakerates_FINAL.root");
+
+  std::vector< std::pair<TString,std::vector<TString>> > samples;
+  std::vector<TString> data_MET;
+  data_MET.push_back("MET_Run2016");
+
+  std::vector<TString> trueTaus;
+  trueTaus.push_back("TTJets_13TeV-powheg");
+  trueTaus.push_back("ST_t-channel_top_4f_inclusiveDecays_13TeV-powheg");
+  trueTaus.push_back("ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powheg");
+  trueTaus.push_back("ST_tW_top_5f_inclusiveDecays_13TeV-powheg");
+  trueTaus.push_back("ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg");
+  trueTaus.push_back("VVTo2L2Nu_13TeV_amcatnloFXFX");
+  trueTaus.push_back("WWToLNuQQ_13TeV_powheg");
+  trueTaus.push_back("WZTo1L1Nu2Q_13TeV_amcatnloFXFX");
+  trueTaus.push_back("WZTo1L3Nu_13TeV_amcatnloFXFX");
+  trueTaus.push_back("WZTo2L2Q_13TeV_amcatnloFXFX");
+  trueTaus.push_back("DYJetsToLL_M-50_13TeV-madgraphMLM");
+  trueTaus.push_back("ZJetsToNuNu_HT-100To200_13TeV-madgraph");
+  trueTaus.push_back("ZJetsToNuNu_HT-200To400_13TeV-madgraph");
+  trueTaus.push_back("ZJetsToNuNu_HT-400To600_13TeV-madgraph");
+  trueTaus.push_back("ZJetsToNuNu_HT-600To800_13TeV-madgraph");
+
+  std::vector<TString> fakeTaus;
+  fakeTaus.push_back("MET_Run2016");
+
+  std::vector<TString> WToTauNu;
+  WToTauNu.push_back("WToTauNu_M-200_13TeV-pythia8");
+  
+  samples.push_back(make_pair("TrueTaus" , trueTaus));
+  samples.push_back(make_pair("FakeTaus" , fakeTaus));
+  samples.push_back(make_pair("WToTauNu" , WToTauNu));
+  samples.push_back(make_pair("data" , data_MET));
+
+  TString var = "mttau";
+
+  for(unsigned int idx_iso=0; idx_iso<iso.size(); idx_iso++){
+
+    cout<<endl<<"-------------------"<<iso[idx_iso]<<"-----------------"<<endl;
+
+    THStack *stack = new THStack(iso[idx_iso],"");
+    TH1D* h_data = 0;
+    std::map<TString,TH1D*> histoMap;
+
+    for (unsigned int i=0; i<samples.size(); ++i) {
+
+      cout<<"Process "<<samples[i].first<<endl;
+    
+      TH1D* histoSamples = new TH1D(samples[i].first,"",10,0,1000);
+      
+      for(unsigned int idx_list=0; idx_list<samples[i].second.size(); idx_list++){
+
+	cout<<"Sample : "<<samples[i].second[idx_list]<<endl;
+
+	TH1D* histo = new TH1D(samples[i].second[idx_list],samples[i].second[idx_list],10,0,1000);
+	selectionCuts select = sr_trueTaus;
+	if(samples[i].first.Contains("FakeTaus")) select =  cr_antiiso;
+
+	makeSelection(dir+"/"+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,histo,var,var,var);
+	histoSamples->Add(histo);
+	histoSamples->SetFillStyle(1001);
+	if(samples[i].first.Contains("FakeTaus")) histoSamples->SetFillColor(TColor::GetColor("#FFCCFF"));
+	else if(samples[i].first.Contains("TrueTaus")) histoSamples->SetFillColor(TColor::GetColor("#6F2D35"));
+	else if(samples[i].first.Contains("WToTauNu")) histoSamples->SetFillColor(TColor::GetColor("#FFCC66"));
+      }
+     
+      if(!samples[i].first.Contains("data")){
+	stack->Add(histoSamples);
+	histoMap[samples[i].first] = histoSamples;
+      }
+      else h_data = (TH1D*) histoSamples->Clone(); 
+      cout<<samples[i].first<<" = "<<histoSamples->Integral()<<endl<<endl;
+    }
+
+    TH1D * bkgdErr = (TH1D*)stack->GetStack()->Last()->Clone("bkgdErr");
+    bkgdErr->SetFillStyle(3013);
+    bkgdErr->SetFillColor(1);
+    bkgdErr->SetMarkerStyle(21);
+    bkgdErr->SetMarkerSize(0);  
+
+    TCanvas * canv = new TCanvas("canv","",700,800);
+    TPad* upper = new TPad("upper","pad",0,0.29,1,1);
+    upper->Draw();
+    upper->cd();
+
+    stack->Draw("hist");
+    stack->GetYaxis()->SetRangeUser(0,1200);
+    stack->GetXaxis()->SetTitle("m_{T} [GeV]");
+    stack->GetYaxis()->SetTitle("Events");
+    gPad->Modified(); 
+    if(h_data){
+      h_data->Draw("e1 same");
+      bkgdErr->Draw("e2same");
+    }
+    
+    TLegend * leg = new TLegend(0.55,0.4,0.85,0.78);
+    SetLegendStyle(leg);
+    leg->SetTextSize(0.047);
+    leg->SetHeader(iso[idx_iso]+"Iso");
+    if(h_data) leg->AddEntry(h_data,"Data","lp");
+    if(histoMap["WToTauNu"]) leg->AddEntry(histoMap["WToTauNu"],"W#rightarrow#tau#nu","f");
+    if(histoMap["FakeTaus"]) leg->AddEntry(histoMap["FakeTaus"],"bkgd (fake taus)","f");
+    if(histoMap["TrueTaus"]) leg->AddEntry(histoMap["TrueTaus"],"bkgd (true taus)","f");
+    writeExtraText = true;
+    extraText = "Internal";
+    CMS_lumi(upper,4,33); 
+    leg->Draw("same");
+
+    TH1D * ratioH = 0;
+    if(h_data){
+      ratioH = (TH1D*) h_data->Clone("ratioH");
+      ratioH->Divide((TH1D*)stack->GetStack()->Last());
+      ratioH->GetYaxis()->SetTitle("obs/exp");
+      ratioH->GetXaxis()->SetTitle("");
+      ratioH->GetYaxis()->SetRangeUser(0.4,1.6);
+    }
+    //TH1D * ratioErrH = (TH1D*)bkgdErr->Clone("ratioErrH");
+   
+    
+    canv->cd();
+    TPad * lower = new TPad("lower", "pad",0,0,1,0.30);
+    lower->SetGridy();
+    lower->Draw();
+    lower->cd();
+    if(h_data) ratioH->Draw("e1");
+    //ratioErrH->Draw("same");
+    canv->cd();
+    canv->Modified();
+    canv->SetSelected(canv);
+    canv->Update();
+    canv->Print("figures/mttau_"+iso[idx_iso]+".png");
+  }
+
+}
