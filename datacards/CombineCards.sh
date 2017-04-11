c003a036e60e6f06b39dd7c99b11be4cd0cc018e
Teresa <teresa.lenz@desy.de>
2017-04-11 09:29:07 +0200

    change naming of files

--- datacards/CombineCards.sh
+++ datacards/CombineCards.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_LooseMvaIso_WTauNu.txt > tauId_LooseMvaIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_MediumMvaIso_WTauNu.txt > tauId_MediumMvaIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_TightMvaIso_WTauNu.txt > tauId_TightMvaIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_VTightMvaIso_WTauNu.txt > tauId_VTightMvaIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_LooseIso_WTauNu.txt > tauId_LooseIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_MediumIso_WTauNu.txt > tauId_MediumIso.txt
-combineCards.py ../output/mtmuon_WMuNu.txt ../output/mttau_TightIso_WTauNu.txt > tauId_TightIso.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_LooseMva_WToTauNu.txt > datacard_LooseMva_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_MediumMva_WToTauNu.txt > datacard_MediumMva_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_TightMva_WToTauNu.txt > datacard_TightMva_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_VTightMva_WToTauNu.txt > datacard_VTightMva_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_Loose_WToTauNu.txt > datacard_Loose_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_Medium_WToTauNu.txt > datacard_Medium_Combined.txt
+combineCards.py ../output/datacards/datacard_mtmuon_WToMuNu.txt ../output/datacards/datacard_mttau_Tight_WToTauNu.txt > datacard_Tight_Combined.txt
 
