621adcb047a96fc5841708bdc3b23341f24e33cc
Teresa <teresa.lenz@desy.de>
2017-04-20 16:01:07 +0200

    better explanations

--- runFullTauIDmeasurement.sh
+++ runFullTauIDmeasurement.sh
@@ -5,26 +5,22 @@ root -l -b -q PlotTrigger.C
 
 # Compute fake rates in W+jets and dijets events and combine them 
 root -l -b -q ComputeFakeRate.C
-root -l -b -q CalculateEWKfraction.C
+root -l -b -q CalculateEWKfraction.C   # This script calculates the EWK fraction and combines the SingleMu and JetHT fake rates
 
-# Run W -> tau nu and W -> mu nu measurements and create a root file with all relevant systematics
+# Run W -> tau nu and W -> mu nu measurements and create a root file including all relevant systematics
 root -l -b -q WToTauNuMeasurement.C
 root -l -b -q WToMuNuMeasurement.C
 
-# Make datacards and combine them
-iniCMSSW_7_4_7
+# Make datacards and run Combine
 root -l -b -q DatacardProducer_WToTauNu.C
 root -l -b -q DatacardProducer_WToMuNu.C
+iniCMSSW_7_4_7
 cd datacards
-source CombineCards.sh
-./Fitting.csh VTightMva
-./Fitting.csh TightMva
-./Fitting.csh MediumMva
-./Fitting.csh LooseMva
-./Fitting.csh Tight
-./Fitting.csh Medium
-./Fitting.csh Loose
+source RunCombine.sh
 cd ..
 
 # Make postfit plots 
 root -l -b -q MakePostFitPlots.C
+
+# In case you want to do a measurement seperately in the tau decay modes, you need to specify the tau decay mode in setting.h (variable tauDecayMode) and in RunCombine.sh
+# Afterwards run all relevant steps again.
