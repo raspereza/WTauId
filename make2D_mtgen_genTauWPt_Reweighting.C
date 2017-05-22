49e6e9ba4a36f8eca64534a09434e0f10dc114c5
Teresa <teresa.lenz@desy.de>
2017-05-22 15:31:57 +0200

    make it flexible to take all WP

--- make2D_mtgen_genTauWPt_Reweighting.C
+++ make2D_mtgen_genTauWPt_Reweighting.C
@@ -5,13 +5,15 @@
 #include "THStack.h"
 #include "TLegend.h"
 
-void make2D_mttau_pttau_Reweighting() {
+void make2D_mtgen_genTauWPt_Reweighting() {
 
   SetStyle();
   TH1::SetDefaultSumw2();
   TH2::SetDefaultSumw2();
 
+  loadWorkingPoints();
   initCuts();
+  loadFakeRates("output/FakeRates_FinerBinning/fakerates" + tauDecayMode + "_FinerBinning.root");
 
   std::vector< std::pair<TString,std::vector<TString>> > samples;
 
@@ -29,45 +31,49 @@ void make2D_mttau_pttau_Reweighting() {
 
   std::map<TString,TH2D*> histoMap;
 
-  for (unsigned int i=0; i<samples.size(); ++i) {
+  for(unsigned int idx_iso=0; idx_iso<iso.size(); idx_iso++){
     
-    cout<<"Process "<<samples[i].first<<endl;
+    for (unsigned int i=0; i<samples.size(); ++i) {
     
-    TH2D* histoSamples = new TH2D(samples[i].first,samples[i].first,nbins1,bins1,nbins2,bins2);
+      cout<<"Process "<<samples[i].first<<endl;
     
-    for(unsigned int idx_list=0; idx_list<samples[i].second.size(); idx_list++){
+      TH2D* histoSamples = new TH2D(samples[i].first,samples[i].first,nbins1,bins1,nbins2,bins2);
+    
+      for(unsigned int idx_list=0; idx_list<samples[i].second.size(); idx_list++){
 
-      cout<<".............. Sample : "<<samples[i].second[idx_list]<<endl;
+	cout<<".............. Sample : "<<samples[i].second[idx_list]<<endl;
 
-      TH2D* histo = new TH2D(samples[i].second[idx_list],samples[i].second[idx_list],nbins1,bins1,nbins2,bins2);
-      selectionCuts select = sr_trueTaus;
-      makeSelection(dir+"/"+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),"Tight",select,histo,var1,var1,var2);
-      histoSamples->Add(histo);
+	TH2D* histo = new TH2D(samples[i].second[idx_list],samples[i].second[idx_list],nbins1,bins1,nbins2,bins2);
+	selectionCuts select = sr_trueTaus;
+	makeSelection(dir+"/"+samples[i].second[idx_list]+".root","NTuple",getXSec(samples[i].second[idx_list]),iso[idx_iso],select,histo,var1,var1,var2);
+	histoSamples->Add(histo);
+      }
+
+      histoMap[samples[i].first] = histoSamples;
+      cout<<samples[i].first<<" = "<<histoSamples->GetSumOfWeights()<<" ( Entries = "<<histoSamples->GetEntries()<<" )"<<endl<<endl;
     }
+    
+    TCanvas * canv = new TCanvas("canv","",1000,700);
+    canv->SetRightMargin(0.17);
+  
+    TH2D *weights = 0;
+    weights = (TH2D*) histoMap["W"]->Clone("weights");
+    weights->SetTitle("");
+    weights->GetXaxis()->SetTitle(var1);
+    weights->GetYaxis()->SetTitle(var2);
 
-    histoMap[samples[i].first] = histoSamples;
-    cout<<samples[i].first<<" = "<<histoSamples->GetSumOfWeights()<<" ( Entries = "<<histoSamples->GetEntries()<<" )"<<endl<<endl;
+    gStyle->SetPaintTextFormat( "1.3f" ); 
+    gStyle->SetTextFont(42);
+    //weights->SetMarkerStyle(20);
+    weights->SetMarkerSize(1.2);
+    weights->Draw("TEXT E COLZ");
+    canv->Print("figures/NumberOfEvents_" + var1 + "_" + var2 + "_WToTauNu_" + iso[idx_iso] + ".pdf");
+
+    // Save histogram in file
+    TFile *out = new TFile("output/ReweightingWeights_" + var1 + "_" + var2 + "_WToTauNu" = iso[idx_iso] + ".root","RECREATE");
+    out->cd();
+    weights->Write("weights");
+
   }
 
-
-  TCanvas * canv = new TCanvas("canv","",1000,700);
-  canv->SetRightMargin(0.17);
-  
-  TH2D *weights = 0;
-  weights = (TH2D*) histoMap["W"]->Clone("weights");
-  weights->SetTitle("");
-  weights->GetXaxis()->SetTitle(var1);
-  weights->GetYaxis()->SetTitle(var2);
-
-  gStyle->SetPaintTextFormat( "1.3f" ); 
-  gStyle->SetTextFont(42);
-  //weights->SetMarkerStyle(20);
-  weights->SetMarkerSize(1.2);
-  weights->Draw("TEXT E COLZ");
-  canv->Print("figures/ReweightingWeights_" + var1 + "_" + var2 + "_WToTauNu.pdf");
-
-  // Save histogram in file
-  TFile *out = new TFile("output/ReweightingWeights_" + var1 + "_" + var2 + "_WToTauNu.root","RECREATE");
-  out->cd();
-  weights->Write("weights");
 }
